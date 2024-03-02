2cd883581738df7b02da6f79954fa5aec6cf4e36
shama <shama.nigar05@gmail.com>
2024-03-01 21:43:27 -0500

    Comment Resource Update API Implementation Changes.

--- src/main/java/com/bharath/learning/blog/socialmediablogapp/Service/Imple/CommentService.java
+++ src/main/java/com/bharath/learning/blog/socialmediablogapp/Service/Imple/CommentService.java
@@ -1,10 +1,15 @@
 package com.bharath.learning.blog.socialmediablogapp.Service.Imple;
 
 import com.bharath.learning.blog.socialmediablogapp.Dto.CommentDto;
+import com.bharath.learning.blog.socialmediablogapp.Dto.PatchDto;
 
 import java.util.List;
 
 public interface CommentService {
     CommentDto createComment(long postid,CommentDto commentDto);
     List<CommentDto> getAllCommentByPostId(long postId);
+    CommentDto getCommentByPostIdAndCommentId(long postId,long id);
+    CommentDto updateCommentByPostIdAndCommentId(long postId,long id,CommentDto commentDto);
+
+    CommentDto updateCommentPartaillyByPostIdAndCommentId(Long postId, Long id, PatchDto patchDto);
 }
